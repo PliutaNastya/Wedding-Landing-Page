0054f2fcba1aaa9a4706abc0eb0baed5acf7d3ea
Anastasiia <koicheva1996@gmail.com>
2022-10-04 11:29:12 +0300

    fix: burger menu

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function burgerMenu() {
-    const burger = document.querySelector('.menu__burger')
-    const menu = document.querySelector('.menu__list')
-    const body = document.querySelector('body')
+const burger = document.querySelector('.menu__burger')
+const menu = document.querySelector('.menu__list')
+const body = document.querySelector('body')
+
+document.addEventListener('DOMContentLoaded', () => {
     burger.addEventListener('click', () => {
-        menu.classList.toggle('active')
-        burger.classList.toggle('active-burger')
-        body.classList.toggle('locked')
+        menu.classList.add('active')
+        burger.classList.add('active-burger')
+        body.classList.add('locked')
     })
-  }
-  burgerMenu()
+});
+
+let links = document.querySelectorAll(".menu__link");
+
+for (let link of links) {
+	link.onclick = () => {
+		body.classList.remove("locked")
+        menu.classList.remove('active')
+        burger.classList.remove('active-burger')
+	}
+}
